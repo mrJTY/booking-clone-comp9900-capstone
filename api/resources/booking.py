54076db130bd79490bf6e1bfcfeeb726d3b4229d
Harris Phan <41245891+statsgrad297@users.noreply.github.com>
2021-07-08 21:18:57 +1000

    Update endpoint for the backend (#56)
    Highly detailed as it imposes the 3 day limit and the 10 hours in a calendar month limit/ @sakshamyadav You can use this as a base to get the 10 hour a calendar month limit.

--- api/resources/booking.py
+++ api/resources/booking.py
@@ -1,6 +1,6 @@
 import json
 import logging
-
+from datetime import datetime, timezone
 from api import db
 from api.resources.availability import AvailabilityModel
 from api.utils.req_handling import *
@@ -8,6 +8,7 @@ from flask_login import current_user
 from flask_restplus import Resource, fields
 from sqlalchemy.orm.attributes import flag_modified
 import api
+
 
 booking = api.api.namespace("bookings", description="booking operations")
 
@@ -76,25 +77,79 @@ class Booking(Resource):
     #     db.session.commit()
     #     return b
 
-    # TODO(Harris/Saksham): Update & Delete
-    # @booking.doc(description=f"booking_id must be provided")
-    # @booking.marshal_with(booking_details)
-    # def put(self, booking_id):
-    #     logging.info(f"Updating booking {booking_id}")
-    #     # get booking id
-    #     content = get_request_json()
-    #     b = BookingModel.query.get_or_404(booking_id)
-    #     # update the booking data
-    #     b.booking_name = content["booking_name"]
-    #     b.address = content["address"]
-    #     b.category = content["category"]
-    #     b.description = content["description"]
-    #     b.user_id = current_user.user_id
-    #     flag_modified(booking, "description")
-    #     db.session.merge(b)
-    #     db.session.flush()
-    #     db.session.commit()
-    #     return b
+    @booking.doc(description=f"booking_id must be provided")
+    @booking.marshal_with(booking_details)
+    def put(self, booking_id):
+        logging.info(f"Updating booking {booking_id}")
+        # get booking id
+        content = get_request_json()
+        try:
+            new_time = AvailabilityModel.query.get_or_404(
+                content["availability_id"]
+            ).to_dict()
+
+            new_time_month = datetime.fromtimestamp(new_time["start_time"]).strftime(
+                "%m"
+            )
+            b = BookingModel.query.get_or_404(booking_id).to_dict()
+            old_time = AvailabilityModel.query.get_or_404(
+                b["availability_id"]
+            ).to_dict()
+            current_unixtime = datetime.now().strftime("%s")
+            # No updated booking is allowed if the difference between now and start date is less than 3 days
+            if (
+                (float(old_time["start_time"]) - float(current_unixtime))
+                / (60 * 60 * 24)
+            ) < 3.0:
+                raise Exception(
+                    "Cannot make an updated booking less than 3 days after the start date of the existing booking"
+                )
+            # New booking cannot allow a user more than 10 hours in a month - note that this is based on the consumer
+            list_bookings = BookingModel.query.filter_by(
+                user_id=current_user.user_id
+            ).all()
+            list_bookings = [l.to_dict() for l in list_bookings]
+            hours_booked = 0
+
+            ##Search through the existing bookings
+            for i in range(len(list_bookings)):
+                timeslot = AvailabilityModel.query.get_or_404(
+                    list_bookings[i]["availability_id"]
+                ).to_dict()
+                if (
+                    datetime.fromtimestamp(timeslot["start_time"]).strftime("%m")
+                    == new_time_month
+                ):
+                    get_interval = (
+                        float(timeslot["end_time"]) - float(timeslot["start_time"])
+                    ) / (60 * 60)
+                    hours_booked += get_interval
+            get_new_booking_interval = (
+                float(new_time["end_time"]) - float(new_time["start_time"])
+            ) / (60 * 60)
+            if (hours_booked + get_new_booking_interval) > 10.0:
+                raise Exception(
+                    "Not allowed to have more than 10 hours in a calendar month"
+                )
+            b1 = BookingModel.query.get_or_404(booking_id)
+            old_avail = AvailabilityModel.query.get_or_404(b["availability_id"])
+            b1.user_id = content["user_id"]
+            # update the booking data
+            b1.listing_id = content["listing_id"]
+            b1.availability_id = content["availability_id"]
+            # Not only this, but we need to update the avaliability_id details as well
+            flag_modified(b1, "availability_id")
+            db.session.merge(b1)
+            # Note - update the old avaliability such that it will go back to true
+            old_avail.is_available = True
+            flag_modified(old_avail, "is_available")
+            db.session.merge(old_avail)
+            db.session.flush()
+            db.session.commit()
+            return b1
+        except Exception as e:
+            logging.error(e)
+            api.api.abort(500, f"{e}")
 
 
 @booking.route("")
--- api/tests/test_booking.py
+++ api/tests/test_booking.py
@@ -26,10 +26,43 @@ LISTING = {
 }
 
 AVAILABILITY = {
-    # Fill this later once you create a listing
-    "listing_id": None,
-    "start_time": 1625214000,
-    "end_time": 162521500,
+    # Fill this later once you create a listing - 5
+    "listing_id": 5,
+    # Unix Epoch time (9am-10am)
+    "start_time": 1625612400,
+    "end_time": 1625616000,
+}
+
+AVAILABILITY_2 = {
+    # Fill this later once you create a listing - 5
+    "listing_id": 5,
+    # Unix Epoch time (10am-11am) - 3 days start time
+    "start_time": 1625616000,
+    "end_time": 1625619600,
+}
+
+AVAILABILITY_3 = {
+    # Fill this later once you create a listing - 5
+    "listing_id": 5,
+    # Unix Epoch time (10am-12pm) - 11th July 2021
+    "start_time": 1625961600,
+    "end_time": 1625968800,
+}
+
+AVAILABILITY_4 = {
+    # Fill this later once you create a listing - 5
+    "listing_id": 5,
+    # Unix Epoch time (10am-10pm) - 12th July 2021 - 12 hour block
+    "start_time": 1626048000,
+    "end_time": 1626091200,
+}
+
+AVAILABILITY_5 = {
+    # Fill this later once you create a listing - 5
+    "listing_id": 5,
+    # Unix Epoch time (8pm-10pm) - 13th July 2021 - 2 hour block but later - should be approved
+    "start_time": 1626170400,
+    "end_time": 1626177600,
 }
 
 
@@ -45,9 +78,65 @@ def test_create_booking():
     # Resource owner creates a listing and availability
     listing_id = u.create_listing(LISTING, owner_token)
     availability_id = u.create_availability(AVAILABILITY, listing_id, owner_token)
-
+    availability_id_2 = u.create_availability(AVAILABILITY_2, listing_id, owner_token)
+    availability_id_3 = u.create_availability(AVAILABILITY_3, listing_id, owner_token)
+    availability_id_4 = u.create_availability(AVAILABILITY_4, listing_id, owner_token)
+    availability_id_5 = u.create_availability(AVAILABILITY_5, listing_id, owner_token)
     # Consumer makes a booking
     booking_id = u.create_booking(
         consumer_user_id, listing_id, availability_id, consumer_token
     )
     assert type(booking_id) == int
+    # Changed their mind - they want the other timeslot (10am-11am). But since its inside 3 days they cannot do it
+    BOOKING_CHANGE_ATTEMPT = {
+        "booking_id": booking_id,
+        "user_id": consumer_user_id,
+        "listing_id": listing_id,
+        "availability_id": availability_id_2,
+    }
+    booking_url = f"{API_URL}/bookings/{booking_id}"
+    response = requests.put(
+        booking_url,
+        json=BOOKING_CHANGE_ATTEMPT,
+        headers={
+            "Authorization": f"JWT {consumer_token}",
+        },
+    )
+    assert response.status_code == 500
+    # Also need to test for over 10 hours a month - they cannot proceed as well. Requires a different booking ID
+    booking_id_2 = u.create_booking(
+        consumer_user_id, listing_id, availability_id_3, consumer_token
+    )
+
+    BOOKING_CHANGE_ATTEMPT_2 = {
+        "booking_id": booking_id_2,
+        "user_id": consumer_user_id,
+        "listing_id": listing_id,
+        "availability_id": availability_id_4,
+    }
+    booking_url = f"{API_URL}/bookings/{booking_id_2}"
+    response = requests.put(
+        booking_url,
+        json=BOOKING_CHANGE_ATTEMPT_2,
+        headers={
+            "Authorization": f"JWT {consumer_token}",
+        },
+    )
+    assert response.status_code == 500
+
+    # Now this one should be approved
+    BOOKING_CHANGE_ATTEMPT_3 = {
+        "booking_id": booking_id_2,
+        "user_id": consumer_user_id,
+        "listing_id": listing_id,
+        "availability_id": availability_id_5,
+    }
+    booking_url = f"{API_URL}/bookings/{booking_id_2}"
+    response = requests.put(
+        booking_url,
+        json=BOOKING_CHANGE_ATTEMPT_3,
+        headers={
+            "Authorization": f"JWT {consumer_token}",
+        },
+    )
+    assert response.status_code == 500
